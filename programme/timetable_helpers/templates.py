52b751c2cb4450b534aa56ef91f0a1a9c7cdc148
jhdark <james.dark.95@gmail.com>
2025-11-14 13:17:44 -0500

    add third day, lower heading of parallel sessions

--- programme/timetable_helpers/templates.py
+++ programme/timetable_helpers/templates.py
@@ -1,7 +1,7 @@
 from textwrap import dedent
 
 
-template = dedent(
+schedule_template = dedent(
     """---
 title: "OSSFE 2026 Programme"
 subtitle: "OSSFE 2026 technical programme committee"
@@ -26,15 +26,19 @@ exports:
     template: ../template
 site:
   hide_toc: false
+  hide_footer_links : false
 ---
 
 Here you will find the schedule and abstracts for the OSSFE 2026 conference
 
-# Tuesday 
+# Tuesday
 {tables_day_1}
 
-# Wednesday 
+# Wednesday
 {tables_day_2}
+
+# Thursday 
+{tables_day_3}
 """
 )
 
@@ -71,7 +75,7 @@ Number of presentations: {num_presentations}
 
 oral_session = dedent(
     """\
-## Oral Session {session_id}: {time_slot}
+### Oral Session {session_id}
 
 Room: {room}
 
@@ -113,7 +117,7 @@ A full list of the posters and their abstracts can be found in the [List of post
 
 tutorial_session = dedent(
     """\
-## 🛠️ Tutorial Session {session_id}: {time_slot}
+### 🛠️ Tutorial Session {session_id}
 
 Room: {room}
 
@@ -152,5 +156,35 @@ lunch_template = dedent(
 
 Lunch is provided in the Munich Urban Colab restaurant (ground floor) for all conference attendees.
 
+"""
+)
+
+lunch_template_proxima = dedent(
+    """\
+## 🍽️ Lunch break: {time_slot}
+
+Lunch is provided in the Proxima Fusion office for all Hackathon attendees.
+
+"""
+)
+
+proxima_tour = dedent(
+    """\
+## 🚶 Proxima Fusion tour: {time_slot}
+
+Join us for an optional social event and get a behind-the-scenes look at Proxima Fusion's facilities.
+"""
+)
+
+
+hackathon = dedent(
+    """\
+## 🖳 OSSFE Hackathon
+
+An optional add-on event for conference attendees. 
+This will be an oppertunity to interact work together with open-source developers on projects of interest and see the
+contiribution process first-hand.
+
+*Structure and details to be announced*
 """
 )
